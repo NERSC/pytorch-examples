1225e6e907136ee832e143fe13b890e7c8132401
Steve Farrell <sparticlesteve@gmail.com>
2018-10-03 14:25:28 -0700

    adding hep-cnn dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -9,5 +9,8 @@ def get_datasets(name, **data_args):
     elif name == 'cifar10':
         from .cifar10 import get_datasets
         return get_datasets(**data_args)
+    elif name == 'hep_images':
+        from .hep_images import get_datasets
+        return get_datasets(**data_args)
     else:
         raise Exception('Dataset %s unknown' % name)
---
+++ datasets/hep_images.py
@@ -0,0 +1,30 @@
+"""
+ATLAS Delphes images for RPV (''HEP-CNN'') classification task.
+"""
+
+# Externals
+import numpy as np
+import h5py
+import torch
+from torch.utils.data import TensorDataset
+
+def _load_file(filename, n_samples):
+    """Load one file from the dataset"""
+    with h5py.File(filename, 'r') as f:
+        data_group = f['all_events']
+        data = data_group['hist'][:n_samples][:,None,:,:].astype(np.float32)
+        labels = data_group['y'][:n_samples].astype(np.float32)
+        weights = data_group['weight'][:n_samples].astype(np.float32)
+    return data, labels, weights
+
+def get_dataset(input_file, n_samples, include_weights=False):
+    """Prepare a dataset for one file"""
+    x, y, w = _load_file(input_file, n_samples)
+    np_inputs = [x, y, w] if include_weights else [x, y]
+    tensors = [torch.from_numpy(a) for a in np_inputs]
+    return TensorDataset(*tensors)
+
+def get_datasets(train_file, valid_file, n_train, n_valid):
+    train_dataset = get_dataset(train_file, n_train)
+    valid_dataset = get_dataset(valid_file, n_valid)
+    return train_dataset, valid_dataset
