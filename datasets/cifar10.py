85c1d525c7537fe57e87ddc3cce63cbf57fb1502
Steve Farrell <sparticlesteve@gmail.com>
2018-10-03 23:12:50 -0700

    added options to take subset of cifar10 datasets

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -6,10 +6,12 @@ PyTorch dataset code for Cifar10
 import os
 
 # Externals
+import torch
+from torch.utils.data import Subset
 import torchvision
 import torchvision.transforms as transforms
 
-def get_datasets(data_path):
+def get_datasets(data_path, n_train=None, n_valid=None):
     """
     Get the training and test datasets
     """
@@ -19,6 +21,17 @@ def get_datasets(data_path):
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     trainset = torchvision.datasets.CIFAR10(root=data_path, train=True,
                                             download=True, transform=transform)
-    testset = torchvision.datasets.CIFAR10(root=data_path, train=False,
-                                           download=True, transform=transform)
-    return trainset, testset
+    validset = torchvision.datasets.CIFAR10(root=data_path, train=False,
+                                            download=True, transform=transform)
+
+    # Generate random subset of the training dataset
+    total_train = len(trainset)
+    if n_train is not None and n_train > 0 and n_train < total_train:
+        trainset = Subset(trainset, torch.randperm(n_train))
+
+    # Generate random subset of the validation dataset
+    total_valid = len(validset)
+    if n_valid is not None and n_valid > 0 and n_valid < total_valid:
+        validset = Subset(validset, torch.randperm(n_valid))
+
+    return trainset, validset
