efd934ca7c253a510dfed77844116a2b23df7296
Steve Farrell <sparticlesteve@gmail.com>
2018-10-04 21:24:10 -0700

    added generic configurable CNN classifier

--- models/__init__.py
+++ models/__init__.py
@@ -9,5 +9,8 @@ def get_model(name, **model_args):
     if name == 'resnet50_cifar10':
         from .resnet_cifar10 import ResNet50
         return ResNet50(**model_args)
+    elif name == 'cnn_classifier':
+        from .cnn_classifier import CNNClassifier
+        return CNNClassifier(**model_args)
     else:
         raise Exception('Model %s unknown' % name)
