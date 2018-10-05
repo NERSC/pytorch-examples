7d9e689c8cc085b8a16ab1f2d4e6805d388b17ca
Steve Farrell <sparticlesteve@gmail.com>
2018-10-04 22:30:32 -0700

    updated script for bigger cifar scaling

--- scripts/submitScaling.sh
+++ scripts/submitScaling.sh
@@ -2,13 +2,16 @@
 
 name=cifar10
 config=configs/${name}.yaml
-nodes="1 2 4 8 16"
+#nodes="1 2 4 8 16"
 #nodes="32 64"
+nodes="128 256 512"
+qos="regular"
+t="10"
 
 mkdir -p logs
 
 set -ex
 for n in $nodes; do
-    sbatch -J $name -N $n -d singleton -o "logs/$name-%j.out" \
+    sbatch -J $name -N $n -q $qos -t $t -d singleton -o "logs/$name-%j.out" \
         scripts/batchScript.sh $config
 done
