0bd2e144a2f160039b74856382a0416c15479197
Steve Farrell <sparticlesteve@gmail.com>
2018-10-15 09:48:09 -0700

    added mnist dataset specification

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,6 +6,9 @@ def get_datasets(name, **data_args):
     if name == 'dummy':
         from .dummy import get_datasets
         return get_datasets(**data_args)
+    elif name == 'mnist':
+        from .mnist import get_datasets
+        return get_datasets(**data_args)
     elif name == 'cifar10':
         from .cifar10 import get_datasets
         return get_datasets(**data_args)
---
+++ datasets/mnist.py
@@ -0,0 +1,24 @@
+"""
+This module contains the code to retrieve the MNIST dataset using the
+existing PyTorch Dataset specification in torchvision.
+"""
+
+# System
+import os
+
+# Externals
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def get_datasets(data_path):
+    data_path = os.path.expandvars(data_path)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    train_dataset = torchvision.datasets.MNIST(root=data_path, train=True,
+                                               download=True, transform=transform)
+    valid_dataset = torchvision.datasets.MNIST(root=data_path, train=False,
+                                               download=True, transform=transform)
+    return train_dataset, valid_dataset
