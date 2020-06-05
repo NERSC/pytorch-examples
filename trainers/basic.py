8d830d653bf18eef8c42e84d1a894c1b93e798a8
Steve Farrell <sparticlesteve@gmail.com>
2020-06-04 17:18:50 -0700

    minor rearrangement of bulds

--- trainers/basic.py
+++ trainers/basic.py
@@ -26,15 +26,15 @@ class BasicTrainer(BaseTrainer):
             device_ids = [self.gpu] if self.gpu is not None else None
             self.model = DistributedDataParallel(self.model, device_ids=device_ids)
 
-        # Construct the optimizer
-        optimizer_config = config['optimizer']
-        Optim = getattr(torch.optim, optimizer_config.pop('name'))
-        self.optimizer = Optim(self.model.parameters(), **optimizer_config)
-
         # Construct the loss function
         loss_config = config['loss']
         Loss = getattr(torch.nn, loss_config.pop('name'))
         self.loss_func = Loss(**loss_config)
+
+        # Construct the optimizer
+        optimizer_config = config['optimizer']
+        Optim = getattr(torch.optim, optimizer_config.pop('name'))
+        self.optimizer = Optim(self.model.parameters(), **optimizer_config)
 
         # Construct the metrics
         metrics_config = config.get('metrics', {})
