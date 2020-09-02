989b2d1ee49badf8a13f00e4360f700789916089
Steve Farrell <sparticlesteve@gmail.com>
2020-09-02 11:35:20 -0700

    update pytorch version

--- scripts/train_cori.sh
+++ scripts/train_cori.sh
@@ -6,10 +6,10 @@
 #SBATCH -o logs/%x-%j.out
 
 # Setup software
-module load pytorch/v1.5.0
+module load pytorch/v1.6.0
 export OMP_NUM_THREADS=32
 export KMP_AFFINITY="granularity=fine,compact,1,0"
-export KMP_BLOCKTIME=1
+export KMP_BLOCKTIME=0
 
 # Run the training
 srun -l -u python train.py -d mpi $@
